ac4d4b541d118a262870bcce63a78c96f16a2bf4
Berkay Gediz <berkaygediz35@gmail.com>
2023-08-15 04:30:33 +0300

    Add project files.

---
+++ wannacrypt0r/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace wannacrypt0r
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern Int32 SystemParametersInfo(UInt32 action, UInt32 uParam, String vParam, UInt32 winIni);
+
+        public void SetWallpaper(String path)
+        {
+            SystemParametersInfo(0x14, 0, path, 0x01 | 0x02);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetWallpaper("");
+            Application.Exit();
+        }
+    }
+}
